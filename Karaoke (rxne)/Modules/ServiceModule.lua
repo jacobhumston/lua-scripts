9ddc5d7202563a0c3cdb0c18fba0849a4fdb2567
Jacob Humston <57332486+jacobhumston@users.noreply.github.com>
2021-10-27 08:47:03 -0400

    Create ServiceModule.lua

---
+++ Karaoke (rxne)/Modules/ServiceModule.lua
@@ -0,0 +1,13 @@
+-- Description: 
+-- Easy access to services, less typing.
+
+-- Usage Example:
+-- local Services = require(<ServiceModule>)
+-- local Workspace = Services.w
+
+return {
+  SSS = game:GetService("ServerScriptService"),
+  SG = game:GetService("StarterGui"),
+  P = game:GetService("Players"),
+  W = game:GetService("Workspace")
+}
